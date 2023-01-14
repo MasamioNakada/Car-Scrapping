b1db8bd6762e35e6081818e19bcdeb5cc378ce33
MasamioNakada <nakada2130@gmail.com>
2023-01-14 14:45:26 -0500

    sys

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import sys
+
 from get_links import get_links
 from process_data import SaveData
 
 if __name__ == "__main__":
-    link_list = get_links(get_all=False)
+    parametro = sys.argv[1]
+    link_list = get_links(get_all=parametro)
     SaveData(link_list).save_csv()
     #SaveData(link_list).save_db()
--- process_data.py
+++ process_data.py
@@ -1,33 +1,33 @@
 import csv  
 import os
-import sqlite3
+#import sqlite3
 
 from extract_from_link import ExtractNeoAutos
 
-conn = sqlite3.connect('database.db')
-cursor = conn.cursor()
-
-cursor.execute('''
-        CREATE TABLE IF NOT EXISTS report(
-            id INTEGER PRIMARY KEY,
-            title TEXT,
-            price REAL,
-            description TEXT,
-            year_manufacture INTEGER,
-            km REAL,
-            transmission TEXT,
-            fuel TEXT,
-            cc REAL,
-            placa TEXT,
-            category TEXT,
-            brand TEXT,
-            model_name TEXT,
-            number_doors INTEGER,
-            traction TEXT,
-            color TEXT,
-            n_cilinders INTEGER
-)''')
-
+#conn = sqlite3.connect('database.db')
+#cursor = conn.cursor()
+#
+#cursor.execute('''
+#        CREATE TABLE IF NOT EXISTS report(
+#            id INTEGER PRIMARY KEY,
+#            title TEXT,
+#            price REAL,
+#            description TEXT,
+#            year_manufacture INTEGER,
+#            km REAL,
+#            transmission TEXT,
+#            fuel TEXT,
+#            cc REAL,
+#            placa TEXT,
+#            category TEXT,
+#            brand TEXT,
+#            model_name TEXT,
+#            number_doors INTEGER,
+#            traction TEXT,
+#            color TEXT,
+#            n_cilinders INTEGER
+#)''')
+#
 class SaveData():
     def __init__(self,link_list:list) -> None:
 
@@ -99,18 +99,19 @@ class SaveData():
             writer = csv.writer(f)
             for report_list in self.report_list_list:
                 writer.writerow(report_list)
-
-    def save_db(self):
-        for report_list in self.report_list_list:
-            print(report_list)
-            cursor.execute(''' INSERT INTO 
-                    report(title,price,description,year_manufacture,km,transmission,fuel,cc,placa,category,brand,model_name,number_doors,traction,color,n_cilinders) 
-                    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
-                    ,report_list)
-
-            conn.commit()
-        conn.close()
         return True
+
+#    def save_db(self):
+#        for report_list in self.report_list_list:
+#            print(report_list)
+#            cursor.execute(''' INSERT INTO 
+#                    report(title,price,description,year_manufacture,km,transmission,fuel,cc,placa,category,brand,model_name,number_doors,traction,color,n_cilinders) 
+#                    VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
+#                    ,report_list)
+#
+#            conn.commit()
+#        conn.close()
+#        return True
    
         
 
@@ -141,34 +142,34 @@ class SaveData():
 #            writer.writerow(list(clean_re.values()))
 #    return True
 
-def save_db(link_list:list):
-    for link in link_list:
-        print(link)
-        ex = ExtractNeoAutos(link)
-        re = ex.get_all()
-        clean_re  = { "title":re['title'],
-                    "price":float(re['price'].replace(",","")), 
-                    "description":re["description"],
-                    "year_manufacture": re["Año Modelo"],
-                    "km":float(re["Kilometraje"][:-3].replace(",","")),
-                    "transmission":re["Transmisión"],
-                    "fuel":re["Combustible"],
-                    "cc":float(re["Cilindrada"][:-3].replace(",","")),
-                    "placa":re["Placa"],
-                    "Categoría":re["Categoría"],
-                    "Marca":re["Marca"],
-                    "Modelo":re["Modelo"],
-                    "Año de fabricación":re["Año de fabricación"],
-                    "Número de puertas":re["Número de puertas"],
-                    "Tracción":re["Tracción"],
-                    "Color":re["Color"],
-                    "Número cilindros":re["Número cilindros"]}
-        cursor.execute(''' INSERT INTO 
-        report(title,price,description,year_manufacture,km,transmission,fuel,cc,placa,category,brand,model_name,year_created,number_doors,traction,color,n_cilinders) 
-        VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
-        ,list(clean_re.values()))
-
-        conn.commit()
-    conn.close()
-    return True
-
+#def save_db(link_list:list):
+#    for link in link_list:
+#        print(link)
+#        ex = ExtractNeoAutos(link)
+#        re = ex.get_all()
+#        clean_re  = { "title":re['title'],
+#                    "price":float(re['price'].replace(",","")), 
+#                    "description":re["description"],
+#                    "year_manufacture": re["Año Modelo"],
+#                    "km":float(re["Kilometraje"][:-3].replace(",","")),
+#                    "transmission":re["Transmisión"],
+#                    "fuel":re["Combustible"],
+#                    "cc":float(re["Cilindrada"][:-3].replace(",","")),
+#                    "placa":re["Placa"],
+#                    "Categoría":re["Categoría"],
+#                    "Marca":re["Marca"],
+#                    "Modelo":re["Modelo"],
+#                    "Año de fabricación":re["Año de fabricación"],
+#                    "Número de puertas":re["Número de puertas"],
+#                    "Tracción":re["Tracción"],
+#                    "Color":re["Color"],
+#                    "Número cilindros":re["Número cilindros"]}
+#        cursor.execute(''' INSERT INTO 
+#        report(title,price,description,year_manufacture,km,transmission,fuel,cc,placa,category,brand,model_name,year_created,number_doors,traction,color,n_cilinders) 
+#        VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''
+#        ,list(clean_re.values()))
+#
+#        conn.commit()
+#    conn.close()
+#    return True
+#
